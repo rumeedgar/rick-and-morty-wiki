13c3ce90ecab1f0d3787a2561ac80b74f41da9f7
rume edgar <rumeedgar@icloud.com>
2023-10-06 18:56:34 +0100

    api updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import Filters from "./components/filters/filters";
 import Cards from "./components/cards/cards";
 
 function App() {
-
   let [pageNumber, setPageNumber] = useState(1);
   let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
-  return <div className="App">
 
-    <h1 className="text-center ubuntu my-5">Rick & Morty <span className="text-primary">wiki</span></h1>
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      console.log(data);
+    })();
+  }, [api]);
 
-    <div className="container">
-      <div className="row">
-        <div className="col-3">
-          <Filters />
-        </div>
-        <div className="col-8">
-          <div className="row">
-            <Cards />
+  return (
+    <div className="App">
+      <h1 className="text-center ubuntu my-5">
+        Rick & Morty <span className="text-primary">wiki</span>
+      </h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters />
+          </div>
+          <div className="col-8">
+            <div className="row">
+              <Cards />
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  );
 }
 
-export default App
+export default App;
