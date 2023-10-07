eccc051f2ede7f8e5244fb0d45ad81d4970e57cb
rume edgar <rumeedgar@icloud.com>
2023-10-07 10:42:15 +0100

    fetching and displaying character names from the api

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -3,7 +3,14 @@ const cards = ({ results }) => {
   console.log(results);
 
   if (results) {
-    display = results.map((x) => {});
+    display = results.map((x) => {
+      let { id, name } = x;
+      return (
+        <div key={id} className="col-4">
+          {name}
+        </div>
+      );
+    });
   } else {
     display = "no characters found :/";
   }
