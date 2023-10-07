4a99494fd1bd3864b943676ea6904aa4754f2012
rume edgar <rumeedgar@icloud.com>
2023-10-07 13:21:40 +0100

    fetched the status of their living from the api and added styling to it

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,20 +1,26 @@
+import styles from "./cards.module.scss";
+
 const cards = ({ results }) => {
   let display;
   console.log(results);
 
   if (results) {
     display = results.map((x) => {
-      let { id, name, image } = x;
+      let { id, name, image, location, status } = x;
       return (
-        <div key={id} className="col-4">
+        <div key={id} className="col-4 position-relative">
           <div className="">
             <img src={image} alt="" className="img-fluid" />
             <div className="content">
               <div className="fs-4 fw-bold mb-4">{name}</div>
               <div className="">
                 <div className="fs-6">last location</div>
+                <div className="fs-5">{location.name}</div>
               </div>
             </div>
+          </div>
+          <div className={`${styles.badge} position-absolute badge bg-danger`}>
+            {status}
           </div>
         </div>
       );
