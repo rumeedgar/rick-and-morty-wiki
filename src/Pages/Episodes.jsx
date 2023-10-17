10bff2557061cdfd82bf1580f72eb4e7c53fb316
rume edgar <rumeedgar@icloud.com>
2023-10-17 19:33:56 +0100

    worked on the episode screen

--- src/Pages/Episodes.jsx
+++ src/Pages/Episodes.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Episodes = () => {
-  return <div>The episodes are here</div>;
+  let [id, setID] = useState(1);
+  let [info, setInfo] = useState([]);
+  let { air_date, name } = info;
+  let api = `https://rickandmortyapi.com/api/episode/${id}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      setInfo(data);
+    })();
+  }, [api]);
+  return (
+    <div className="container">
+      <div className="row">
+        <h1 className="text-center mb-3">Episode : {name}</h1>
+        <h5 className="text-center">Air Date {air_date}</h5>
+      </div>
+      <div className="row"></div>
+    </div>
+  );
 };
 
 export default Episodes;
