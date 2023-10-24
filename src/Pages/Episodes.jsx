d5986011994929910e91727877c2ccd1c9725725
rume edgar <rumeedgar@icloud.com>
2023-10-24 16:53:48 +0100

    making application mobile responsive

--- src/Pages/Episodes.jsx
+++ src/Pages/Episodes.jsx
@@ -34,11 +34,11 @@ const Episodes = () => {
         </h5>
       </div>
       <div className="row mb-4">
-        <div className="col-3">
+        <div className="col-lg-3 col-12">
           <h4 className="text-center mb-4">Pick Episode</h4>
           <InputGroup setID={setID} name="Episode" total={51} />
         </div>
-        <div className="col-8">
+        <div className="col-lg-8 col-12">
           <div className="row">
             <Cards page="/episodes/" results={results} />
           </div>
