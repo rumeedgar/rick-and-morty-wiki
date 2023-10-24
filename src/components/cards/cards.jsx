17db3caa25429563a3fdaff9dc810baf5f3a7ef1
rume edgar <rumeedgar@icloud.com>
2023-10-24 16:49:09 +0100

    making application mobile responsive

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -15,7 +15,9 @@ const cards = ({ results, page }) => {
           key={id}
           className="col-lg-4 col-md-6 col-12 mb-4 position-relative text-dark"
         >
-          <div className={styles.cards}>
+          <div
+            className={`${styles.cards} d-flex flex-column justify-content-center`}
+          >
             <img src={image} alt="" className={`${styles.img} img-fluid`} />
             <div style={{ padding: "10px" }} className="content">
               <div className="fs-4 fw-bold mb-4">{name}</div>
--- src/components/filters/filters.jsx
+++ src/components/filters/filters.jsx
@@ -11,7 +11,7 @@ const filters = ({ setStatus, setPageNumber, setGender, setSpecies }) => {
     window.location.reload(false);
   };
   return (
-    <div className="col-3">
+    <div className="col-lg-3 col-12 mb-5">
       <div className="text-center fw-bold fs-4 mb-2">Filter</div>
       <div
         onClick={clear}
